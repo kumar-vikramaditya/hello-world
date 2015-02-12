6399bda35ec5d9923654983ac543073251cc6d37
Kumar Vikramaditya <vikramaditya.in@gmail.com>
2015-02-11 18:10:41 -0800

    updated with get request

--- src/main/scala/hello/GreetingController.scala
+++ src/main/scala/hello/GreetingController.scala
@@ -2,11 +2,12 @@ package hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
  class GreetingController {
 
-    @RequestMapping(Array("/"))
+    @RequestMapping(value=Array("/"), method=Array(RequestMethod.GET))
     def greeting():String ={
         "Hello World from Spring Boot Scala";
     }
